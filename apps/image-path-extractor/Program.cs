affbf74d291a7736172d1d28c794544c26a65ae8
Nate Sobol <natesobol@gmail.com>
2025-12-12 10:46:23 -0500

    Add image path extractor webapp

---
+++ apps/image-path-extractor/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+
+app.Run();
